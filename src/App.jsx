8eef3ae7c06beacbbd894ed048459e4a0a2e1ac2
ThusharaGit <thushara.weeraparakrama@gmail.com>
2024-03-13 10:47:27 +0100

    copied existing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import data from './assets/data.json'
+
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import FAQs from './components/FAQs'
+import Home from './components/Home'
+import About from './components/About'
+import Trips from './components/Trips'
+import Contact from './components/Contact'
+import Contact2 from './components/Contact2'
+import Footer from './components/Footer'
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+     <Nav/>
+     <Routes>
+     <Route path= '/' element= {<Home parentsComments={data.parentsComents}/>}/>
+      <Route path= 'FAQs' element= {<FAQs headerComponents= {data.headerComponents}/>}/>
+      <Route path= 'About' element= {<About headerComponents= {data.headerComponents}/>}/>
+      <Route path= 'Trips' element= {<Trips headerComponents= {data.headerComponents}/>}/>
+      <Route path= 'Contact' element= {<Contact headerComponents= {data.headerComponents}/>}/>
+      <Route path= 'Contact2' element= {<Contact2 headerComponents= {data.headerComponents}/>}/>
+     </Routes>
+     <Footer/>
+    </div>
   )
 }
 
